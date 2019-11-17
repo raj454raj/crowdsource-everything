aa56edc9f6af9117bf9163d9c789e661f2a8df3e
raj454raj <raj454raj@gmail.com>
2019-11-17 22:14:08 +0530

    finishing up UI a bit

--- app.js
+++ app.js
@@ -3,37 +3,72 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 const app = express();
 
 const port = 3000;
 const questions = {};
+const answers = {};
 
 const getFileName = function(fileName) {
   return path.join(__dirname + '/' + fileName);
 };
 
+const getNextProblem = function(handle) {
+  return {question_id: 6, question_content: "hello world?"};
+};
+
+app.use(bodyParser.json());
 app.use(express.static('static'));
 app.use(morgan('combined'));
 
-fs.createReadStream(getFileName('data/questions.csv'))
-  .pipe(parse())
-  .on('data', (row) => {
-    questions[row[0]] = row[1];
-  })
-  .on('end', () => {
-    console.log(questions);
-  });
 
 app.get('/', function(req, res) {
   res.send('Welcome to Crowdsourcing!');
 });
 
 app.get('/crowdsource', function(req, res) {
+  fs.createReadStream(getFileName('data/questions.csv'))
+    .pipe(parse())
+    .on('data', (row) => {
+      questions[row[0]] = row[1];
+    })
+    .on('end', () => {
+      console.log(questions);
+    });
+
   res.sendFile(getFileName('views/index.html'));
 });
 
+app.get('/get_answers_list', function(req, res) {
+  var promise = new Promise((resolve, reject) => {
+    fs.createReadStream(getFileName('data/answers.csv'))
+      .pipe(parse())
+      .on('data', (row) => {
+        answers[row[0]] = row[1];
+      })
+      .on('end', () => {
+        resolve(answers);
+      });
+  });
+
+  promise.then(function(result) {
+    res.json(answers);
+  });
+});
+
+app.get('/get_next_problem', function(req, res) {
+  console.log("get_next_problem", req.params);
+  res.json(getNextProblem("abcd"));
+});
+
 app.post('/submit_vote', function(req, res) {
-  res.end();
+  console.log(req.body);
+  console.log(req.body.handle);
+  if (req.body.handle === '') res.json({});
+  console.log("submit_vote", req.body.handle);
+  // Redis set operations
+  res.json(getNextProblem("abcd"));
 });
 
 app.listen(port, () => console.log('Listening at port', port));
--- static/crowdsource.js
+++ static/crowdsource.js
@@ -1,6 +1,23 @@
 var githubHandle = "";
 
+var populateQuestionDetails = function(response) {
+  $("#crowdsource-form").attr({"data-question-id": response["question_id"]});
+  $('#crowdsource-question').html(response["question_content"]);
+};
+
+
 $(document).ready(function() {
+  $.ajax({
+    url: "/get_answers_list",
+    method: "GET",
+    success: function(response) {
+      var $content = $('#crowdsource-form-content');
+      $.each(response, function(val, display) {
+        $content.append('<p class="crowdsource-list-item"><label><input name="answer_value" type="radio" value="' + val + '" required/><span>' + display + '</span></label></p>');
+      });
+    }
+  });
+
   $("#submit-github-handle").on('click', function() {
     githubHandle = $("#github-handle").val();
     if (!githubHandle) { // Todo check for valid handles
@@ -8,5 +25,39 @@ $(document).ready(function() {
       return;
     }
     console.log(githubHandle);
+    $.ajax({
+      url: "/get_next_problem",
+      method: "GET",
+      data: {handle: githubHandle},
+      contentType: 'application/json',
+      success: function(response) {
+        populateQuestionDetails(response);
+        $("#github-handle-div").fadeOut();
+        $("#questions-complete-div").fadeIn();
+      }
+    });
+  });
+
+  $('#crowdsource-form').on('submit', function(e) {
+    e.preventDefault();
+    if (!githubHandle) {
+      M.toast({html: 'Please enter your Github handle!'});
+      return;
+    }
+    $.ajax({
+      url: '/submit_vote',
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({question_id: $('#crowdsource-form').data()["questionId"], handle: githubHandle}),
+      success: function(response) {
+        console.log(response);
+        populateQuestionDetails(response);
+        $('#crowdsource-form').trigger('reset');
+      },
+      error: function(err) {
+        console.log(err);
+        M.toast({html: 'Something went wrong!'});
+      }
+    })
   });
 });
